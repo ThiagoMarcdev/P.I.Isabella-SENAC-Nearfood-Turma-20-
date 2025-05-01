1d3cd50c637dbe87d2cbc4d064d2a5f8e0ad91b3
Guilherme Sousa <guifirmo181@gmail.com>
2025-05-01 17:58:58 -0300

    adicionando classe conexão; adicionando conteudo da classe usuario; criando banco de dados sqlserver; NearFoodDB

--- adm_desktop/src/main/java/br/com/nearfood/model/Conexao.java
+++ adm_desktop/src/main/java/br/com/nearfood/model/Conexao.java
@@ -5,7 +5,19 @@ package br.com.nearfood.model;
     nesta classe para facil manutenção e consequentemente facil escalabilidade.
 */
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
+    
+    public static Connection getConexao() throws SQLException {
+final String SERVIDOR = "jdbc:sqlserver://127.0.0.1:1433;databaseName=NearFoodDB;"
++ "encrypt=false;trustServerCertificate=true";
+final String USUARIO = "sa";
+final String SENHA = "Nearfood@2025";
+return DriverManager.getConnection(SERVIDOR, USUARIO, SENHA);
+}
     
     
 }
--- adm_desktop/src/main/java/br/com/nearfood/model/Usuario.java
+++ adm_desktop/src/main/java/br/com/nearfood/model/Usuario.java
@@ -1,8 +1,32 @@
-
 package br.com.nearfood.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Usuario {
-    
-    
+
+    //MÉTODO PARA CADASTRO DE USUÁRIO
+    public void cadastrar(String nome, String email, String senha) throws SQLException {
+        String sql = "INSERT INTO tblUsuarios (nome, email, senha) VALUES (?, ?, ?)";
+        try (Connection con = Conexao.getConexao(); PreparedStatement stmt = con.prepareStatement(sql)) {
+            stmt.setString(1, nome);
+            stmt.setString(2, email);
+            stmt.setString(3, senha);
+            stmt.executeUpdate();
+
+        }
+    }
+
+    //MÉTODO PARA LOGIN DE USUÁRIO
+    public boolean login(String email, String senha) throws SQLException {
+        String sql = "SELECT * FROM tblUsuarios WHERE email = ? AND senha = ?";
+        try (Connection con = Conexao.getConexao(); PreparedStatement stmt = con.prepareStatement(sql)) {
+            stmt.setString(1, email);
+            stmt.setString(2, senha);
+            ResultSet rs = stmt.executeQuery();
+            return rs.next();
+        }
+    }
 }
