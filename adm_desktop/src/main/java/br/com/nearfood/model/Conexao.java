73a8108795721171f2b4037ef5e732dc1287daa8
Guilherme Sousa <guifirmo181@gmail.com>
2025-05-01 23:56:24 -0300

    configurando arquivo xml faltante para conexão com banco de dados

--- adm_desktop/src/main/java/br/com/nearfood/model/Conexao.java
+++ adm_desktop/src/main/java/br/com/nearfood/model/Conexao.java
@@ -4,7 +4,6 @@ package br.com.nearfood.model;
     utilizado. Objetivo é centralizar configurações referentes a isso
     nesta classe para facil manutenção e consequentemente facil escalabilidade.
  */
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,10 +11,10 @@ import java.sql.SQLException;
 public class Conexao {
 
     public static Connection getConexao() throws SQLException {
-        final String SERVIDOR = "jdbc:sqlserver://127.0.0.1:1433;databaseName=NearFoodDB;"
-                + "encrypt=false;trustServerCertificate=true";
+        final String SERVIDOR = "jdbc:sqlserver://127.0.0.1:1433;databaseName=NearFoodDB;encrypt=false;trustServerCertificate=true";
         final String USUARIO = "sa";
-        final String SENHA = "Nearfood@2025";
+        final String SENHA = "NearFood@2025";
+
         return DriverManager.getConnection(SERVIDOR, USUARIO, SENHA);
     }
 
